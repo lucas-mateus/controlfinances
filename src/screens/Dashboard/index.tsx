0a201eb48ba1a324773827d286f63d5d60f7f8ad
lucas-mateus <xlucasmat@gmail.com>
2022-12-07 19:42:12 -0300

    configurando styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+}

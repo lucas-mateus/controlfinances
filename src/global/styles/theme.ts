18797b265d97c9e8e6a1c807a65de91185c2db89
lucas-mateus <xlucasmat@gmail.com>
2022-12-07 23:46:13 -0300

    Configurando estilo global

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,26 @@
+export default {
+  color: {
+    primary: "#12A454",
+    primary_light: "rgba(18, 164, 84, 0.5)",
+
+    attention: "#E83F5B",
+    attention_light: "rgba(232, 63, 91, 0.5)",
+
+    secondary: "#5636D3",
+
+    warning: "#FF872C",
+    warning_light: "rgba(255, 135, 44, 0.5)",
+
+    shape: "#FFFFFF",
+    dark: "#2F323D",
+    title: "#2F323D",
+    text: "#969CB3",
+    background: "#F0F2F5",
+  },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold",
+  },
+};

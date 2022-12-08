ae00d070d6bc2cbef3fa946a8b1420952567f802
lucas-mateus <xlucasmat@gmail.com>
2022-12-07 23:47:05 -0300

    Corrigindo carregamento das fontes

--- App.tsx
+++ App.tsx
@@ -1,16 +1,43 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return <Dashboard />;
-}
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    </View>
+  );
+}

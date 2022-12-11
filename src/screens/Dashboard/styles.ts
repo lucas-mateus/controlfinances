8b05614136bc47eef3489b2f70e3d0e9df7af52f
lucas-mateus <xlucasmat@gmail.com>
2022-12-11 17:44:54 -0300

    Implementando FlatList no styled components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,13 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { DataListProps } from ".";
+
+import {
+  getBottomSpace,
+  getStatusBarHeight,
+} from "react-native-iphone-x-helper";
 
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
@@ -70,3 +76,25 @@ export const HighlightCards = styled.ScrollView.attrs({
   position: absolute;
   margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+  flex: 1;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(12)}px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  margin-bottom: 16px;
+`;
+
+export const TransactionsList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingBottom: getBottomSpace(),
+  },
+})``;

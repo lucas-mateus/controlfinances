d3e96513c2de8d28e9b193ebcc4330987670a2ac
lucas-mateus <xlucasmat@gmail.com>
2022-12-14 01:11:38 -0300

    Criando array da lista de categorias

---
+++ src/utils/categories.ts
@@ -0,0 +1,8 @@
+export const categories = [
+  { key: "purchase", name: "Compras", icon: "shopping-bag", color: "#5636d3" },
+  { key: "food", name: "Comida", icon: "coffee", color: "#ff822c" },
+  { key: "salary", name: "Salário", icon: "dollar-sign", color: "#128454" },
+  { key: "car", name: "Transporte", icon: "truck", color: "#e83f5b" },
+  { key: "leisures", name: "Lazer", icon: "heart", color: "#26195c" },
+  { key: "studies", name: "Estudos", icon: "book", color: "#16d4ff" },
+];

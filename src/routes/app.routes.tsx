fcaf4c492fda33f800db6721430344873575fedd
lucas-mateus <xlucasmat@gmail.com>
2022-12-21 00:26:37 -0300

    Estilizando tab navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Platform } from "react-native";
+import { useTheme } from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
 
@@ -7,10 +10,52 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Listagem" component={Dashboard} />
-      <Tab.Screen name="Cadastrar" component={Register} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.color.secondary,
+        tabBarInactiveTintColor: theme.color.subTitle,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+          height: 88,
+        },
+      }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              size={size}
+              color={color}
+              name="format-list-bulleted"
+            />
+          ),
+        }}
+        name="Listagem"
+        component={Dashboard}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="attach-money" />
+          ),
+        }}
+        name="Cadastrar"
+        component={Register}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons size={size} color={color} name="pie-chart" />
+          ),
+        }}
+        name="Resumo"
+        component={Register}
+      />
     </Tab.Navigator>
   );
 }

c7aa2dbfa0ee53ed760f8c7267910c3b48d68c9c
lucas-mateus <xlucasmat@gmail.com>
2022-12-28 20:31:51 -0300

    fix: conserta a listagem a partir do useFocusEffect

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { HighlightCard } from "../../components/HighlightCard";
 import {
   TransactionCard,
@@ -66,6 +67,12 @@ export function Dashboard() {
   useEffect(() => {
     loadTransactions();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadTransactions();
+    }, [])
+  );
 
   return (
     <Container>

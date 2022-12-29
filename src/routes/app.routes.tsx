34dfdc1bc269fddcb53fbdfa27655a9f2b01499e
lucas-mateus <xlucasmat@gmail.com>
2022-12-29 01:08:50 -0300

    feat: inclui a rota de resumo

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,7 +4,7 @@ import { useTheme } from "styled-components";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
-
+import { Resume } from "../screens/Resume";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
@@ -54,7 +54,7 @@ export function AppRoutes() {
           ),
         }}
         name="Resumo"
-        component={Register}
+        component={Resume}
       />
     </Tab.Navigator>
   );

811a3d011c642b0e706c3a8e3b136f1e5bd2c5b0
lucas-mateus <xlucasmat@gmail.com>
2023-01-03 23:46:59 -0300

    feat: implementa hook de estado

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, createContext, ReactNode } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: "128301928341",
+    name: "Lucas",
+    email: "lucas@gmail.com",
+  };
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
